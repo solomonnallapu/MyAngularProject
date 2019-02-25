e34ee068d7fa1a9970de7e873b7dfb5e50b2fba5
SolomonNallapu <solomon.n@way2online.co.in>
2019-02-25 16:43:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'component1', component: Component1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Practise-Lab';
+  
+  activeCondition = true
+  activefunc(){
+    console.log('Working');
+    this.activeCondition = !this.activeCondition
+  }
 }
